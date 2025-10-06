53deb064a91c5d8778d7afdbba082766a3d54351
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-10-06 16:36:58 +0200

    Init

---
+++ client/tcpchatclient.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+#define BUFFER_SIZE 1024
+
+int port = 8080;
+char name[20];
+
+int main(int argc, char *argv[])
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    char message[BUFFER_SIZE];
+
+    printf("\033[2J\033[H");
+    printf("Enter the port to connect to (default %d): ", port);
+    char port_input[10];
+    scanf("%9s", port_input);
+
+    printf("Enter your name: ");
+    scanf("%19s", name);
+
+    if (strlen(port_input) > 0)
+    {
+        port = atoi(port_input);
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    printf("> ");
+    fflush(stdout);
+
+    while (1)
+    {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        FD_SET(sock, &readfds);
+
+        int max_fd = sock > STDIN_FILENO ? sock : STDIN_FILENO;
+
+        int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+        if (activity < 0)
+        {
+            perror("select error");
+            break;
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &readfds))
+        {
+            fgets(message, BUFFER_SIZE, stdin);
+            message[strcspn(message, "\n")] = 0;
+
+            if (strcmp(message, "exit") == 0)
+            {
+                break;
+            }
+
+            send(sock, message, strlen(message), 0);
+            printf("> ");
+            fflush(stdout);
+        }
+
+        if (FD_ISSET(sock, &readfds))
+        {
+            int valread = read(sock, buffer, BUFFER_SIZE);
+            if (valread > 0)
+            {
+                buffer[valread] = '\0';
+                printf("\r\033[K");
+                printf("%s\n", buffer);
+                printf("> ");
+                fflush(stdout);
+            }
+            else if (valread == 0)
+            {
+                printf("\nServer disconnected\n");
+                break;
+            }
+        }
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ server/tcpchatserver.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+#define MAX_CLIENTS 100
+#define BUFFER_SIZE 1024
+
+int port = 8080;
+
+int main(int argc, char *argv[])
+{
+    int server_fd, new_socket, client_socket[MAX_CLIENTS];
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    fd_set readfds;
+    int max_sd, sd, activity, i, j;
+    socklen_t addrlen;
+
+    for (i = 0; i < MAX_CLIENTS; i++)
+    {
+        client_socket[i] = 0;
+    }
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)))
+    {
+        perror("Setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 5) < 0)
+    {
+        perror("Listen");
+        exit(EXIT_FAILURE);
+    }
+
+    addrlen = sizeof(client_addr);
+    printf("TCP Chat Server listening on port %d\n", port);
+
+    while (1)
+    {
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        for (i = 0; i < MAX_CLIENTS; i++)
+        {
+            sd = client_socket[i];
+            if (sd > 0)
+                FD_SET(sd, &readfds);
+            if (sd > max_sd)
+                max_sd = sd;
+        }
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+        if (activity < 0)
+        {
+            perror("Select error");
+            exit(EXIT_FAILURE);
+        }
+
+        if (FD_ISSET(server_fd, &readfds))
+        {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&addrlen)) < 0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+
+            for (i = 0; i < MAX_CLIENTS; i++)
+            {
+                if (client_socket[i] == 0)
+                {
+                    client_socket[i] = new_socket;
+                    printf("New connection: socket fd is %d, ip is : %s, port : %d\n", new_socket, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    break;
+                }
+            }
+        }
+
+        for (i = 0; i < MAX_CLIENTS; i++)
+        {
+            sd = client_socket[i];
+            if (FD_ISSET(sd, &readfds))
+            {
+                int valread;
+                if ((valread = read(sd, buffer, BUFFER_SIZE)) == 0)
+                {
+                    getpeername(sd, (struct sockaddr *)&client_addr, (socklen_t *)&addrlen);
+                    printf("Host disconnected: ip %s, port %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                    close(sd);
+                    client_socket[i] = 0;
+                }
+                else
+                {
+                    buffer[valread] = '\0';
+                    printf("Received message: %s\n", buffer);
+
+                    // Create formatted message with sender info
+                    char formatted_message[BUFFER_SIZE + 50];
+                    snprintf(formatted_message, sizeof(formatted_message), "Client[%d]: %s", sd, buffer);
+
+                    // Echo back to sender (so they see their own message)
+                    send(sd, formatted_message, strlen(formatted_message), 0);
+
+                    // Broadcast to all other clients
+                    for (j = 0; j < MAX_CLIENTS; j++)
+                    {
+                        if (client_socket[j] != 0 && client_socket[j] != sd)
+                        {
+                            send(client_socket[j], formatted_message, strlen(formatted_message), 0);
+                        }
+                    }
+
+                    // Clear buffer for next message
+                    memset(buffer, 0, BUFFER_SIZE);
+                }
+            }
+        }
+    }
+
+    close(server_fd);
+    return 0;
+}
